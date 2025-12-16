7d0e3297bd7b174147f23297980a1e2afac4e252
Wei-xu293 <falseacc007@gmail.com>
2025-12-16 16:33:00 +0100

    feat: initial navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import './App.css'
-import { UserProvider } from './contexts/UserContext'
-import UserSelector from './components/UserSelector'
-import ProfilePage from './pages/ProfilePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import FeedPage from './pages/FeedPage';
+import CommunitiesPage from './pages/CommunitiesPage';
+import CreatePostPage from './pages/CreatePostPage';
+import ProfilePage from './pages/ProfilePage';
+import { UserProvider } from './contexts/UserContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import './App.css';
 
 function App() {
   return (
     <UserProvider>
-      <UserSelector />
-      <div className="app-container">
-        <ProfilePage />
-      </div>
+      <ThemeProvider>
+        <BrowserRouter>
+          <div className="app-layout">
+            <Navbar />
+            <div className="main-content">
+              <Routes>
+                <Route path="/" element={<FeedPage />} />
+                <Route path="/communities" element={<CommunitiesPage />} />
+                <Route path="/create-post" element={<CreatePostPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+              </Routes>
+            </div>
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
     </UserProvider>
   )
 }
